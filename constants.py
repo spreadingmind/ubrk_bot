55473980544ff34f64c30fe984734d9306652fc9
spreadingmind <lisabeth.shevtsova@gmail.com>
2017-03-22 19:25:06 +0300

    added file

---
+++ constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+api_key = 'xxx'
+api_secret = 'xxx'
+oauth_token  = 'xxx'
+oauth_token_secret = 'xxx'
+
+
+UBRK_list = ['пол на кухне' , 'холвэй', 'кухонные поверхности', 'ванная и туалет', 'мусор', 'расставить посуду']
+
+bot_token = 'xxx'
+
