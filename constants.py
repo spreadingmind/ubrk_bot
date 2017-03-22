86d14d94cd57d4c1d78c1d3d2d831df38d5dda08
spreadingmind <lisabeth.shevtsova@gmail.com>
2017-03-23 00:18:06 +0300

    comment

--- constants.py
+++ constants.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
-api_key = 'xxx'
-api_secret = 'xxx'
-oauth_token  = 'xxx'
-oauth_token_secret = 'xxx'
+api_key = 'd63b54d50702f5668028c3cfe57af002'
+api_secret = '50ced50039855e91c79a5a13f37448b5e0c91721b9d492c4c6de9454d42f310e'
+oauth_token  = 'd509ea1db40a3a735363022d7734ea85c235b5062bee6fc381c1e2c4b4ed8bfa'
+oauth_token_secret = 'dfb4a66da2720ea32823e9054fb5908c'
 
+'''
+My Boards IDs:
+
+<Board EfimYLisa> 57f12561d035b0700def4f5d
+<Board Обустройство> 5797b42f5d6fcf7467b43570
+<Board My way to Programming> 58beca28f32bc7a23a707488
+<Board Welcome Board> 57eb99e4b60861cf2ff79fe2
+<Board Dasha&Lisa Mega Projects> 588de07589a8fda571ab874d
+<Board workshop> 57ddb7439a1a231391a6237f
+<Board Lisa's PROJECT BOARD> 582b56a974dc49ee81d15416
+<Board India> 5884ea3155150c2f7b0562cf
+<Board UBRK is fun> 58c675660114d45a75eb1ce4
+<Board Проекты> 57b00c825a3924daa648ff01
+'''
 
 UBRK_list = ['пол на кухне' , 'холвэй', 'кухонные поверхности', 'ванная и туалет', 'мусор', 'расставить посуду']
 
-bot_token = 'xxx'
+bot_token = '342349054:AAG8Fj69AbPHM4ixF0jCLGsQnagnwBf10NQ'
 
