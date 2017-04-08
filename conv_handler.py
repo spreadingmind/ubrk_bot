a4b9500593f616d9f31db634757608f78e7d36b4
spreadingmind <lisabeth.shevtsova@gmail.com>
2017-04-08 15:05:56 +0300

    issues command added

--- conv_handler.py
+++ conv_handler.py
@@ -211,16 +211,23 @@ def add_issue(bot, update, args):
         print('issue added')
         issues.get_issues_keyboard_list()
         issues.get_issues_keyboard()
-    bot.sendMessage(chat_id=update.message.chat_id, text=emojize('Issue added to list. Cool :clap:', use_aliases=True))
+        bot.sendMessage(chat_id=update.message.chat_id, text=emojize('Issue added to list. Cool :clap:', use_aliases=True))
 
 issue_handler = CommandHandler('issue', add_issue, pass_args=True)
 updater.dispatcher.add_handler(issue_handler)
 
-# def delete_issue(bot, update, args):
-#     issue_to_del = ' '.join(args)
-#     if issue_to_del is not None:
-#         issues.issues_on_board.
+def delete_issue(bot, update, args):
+    issue_to_del = ' '.join(args)
+    if issue_to_del is not None:
+        for card in issues.issues_list:
+            if card.name == issue_to_del
 
+                card.delete()
+                bot.sendMessage(chat_id=update.message.chat_id, text=emojize('Issue removed Great job! :wink:', use_aliases=True))
+        issues.get_issues_keyboard_list()
+
+issue_to_del_handler = CommandHandler('delissue',delete_issue, pass_args=True)
+updater.dispatcher.add_handler(issue_to_del_handler)
 
 
 
